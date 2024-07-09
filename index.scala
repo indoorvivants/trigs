ab8d1a12da83657aeab7f2ebf48e635fbacdbc8e
Anton Sviridov <keynmol@gmail.com>
2024-07-09 09:17:57 +0200

    WIP

--- index.scala
+++ index.scala
@@ -2,11 +2,23 @@
 //> using dep com.lihaoyi::os-lib::0.10.2
 //> using scala 3.5.0-RC2
 import scala.collection.mutable.ArrayBuilder
+import java.nio.ByteBuffer
+import java.io.DataInputStream
 
-opaque type TRIGRAM = String
-object TRIGRAM:
-  def apply(a: Char, b: Char, c: Char): TRIGRAM = s"$a$b$c"
-  given Ordering[TRIGRAM] = Ordering.String
+object types:
+  opaque type TRIGRAM = String
+  object TRIGRAM:
+    def apply(a: Char, b: Char, c: Char): TRIGRAM = s"$a$b$c"
+
+    extension (t: TRIGRAM)
+      def _1: Char = t(0)
+      def _2: Char = t(1)
+      def _3: Char = t(2)
+    given Ordering[TRIGRAM] = Ordering.String
+  end TRIGRAM
+end types
+
+import types.*
 
 class TrigramIndexer:
   def index(s: String): Array[TRIGRAM] =
@@ -99,7 +111,127 @@ case class IndexEntry(tokens: Array[TRIGRAM], location: Location):
   end jaccardSimilarity
 end IndexEntry
 
-@main def index(path: String, extensions: String) =
+object TrigramIndexer
+
+def serialiseIndex(nice: Map[TRIGRAM, Set[Location]]): Array[Byte] =
+  val sorted = nice.toList.sortBy(_._1)
+  val locationsIndex =
+    val builder = collection.mutable.Map.empty[Location, Int]
+    nice.valuesIterator
+      .flatMap(_.toIterator)
+      .foreach: loc =>
+        builder.getOrElseUpdate(loc, builder.size)
+    builder.toMap
+  end locationsIndex
+
+  val trigramIds =
+    val builder = collection.mutable.Map.empty[TRIGRAM, Int]
+    nice.keysIterator.foreach: trigram =>
+      builder.getOrElseUpdate(trigram, builder.size)
+    builder.toMap
+  end trigramIds
+
+  val locationsIndexSizeEstimate = locationsIndex.map { (loc, _) =>
+    val fileNameSize = loc.file.toString.getBytes().length
+    val fileNameFieldLength = 4 // Int?
+    val idLength = 4
+    val lineLength = 4
+    fileNameSize + fileNameFieldLength + idLength + lineLength
+  }.sum
+
+  val locationCountSize = 4
+
+  val trigramsSizeEstimate = trigramIds
+    .map: (trigram, id) =>
+      2 * 3 + 4 + 4
+    .sum
+
+  val trigramCountSize = 4
+
+  val indexSizeEstimate =
+    nice.map((_, locs) => 4 + 4 + locs.size * 4).sum
+
+  val b = ByteBuffer.allocate(
+    locationCountSize + locationsIndexSizeEstimate + trigramCountSize + trigramsSizeEstimate + indexSizeEstimate
+  )
+
+  b.putInt(locationsIndex.size)
+
+  locationsIndex.foreach: (location, id) =>
+    val loc = location.file.toString
+    b.putInt(id)
+    b.putInt(location.line)
+    b.putInt(loc.length)
+    b.put(loc.getBytes())
+
+  b.putInt(trigramIds.size)
+
+  trigramIds.foreach: (trigram, id) =>
+    b.putInt(id)
+    b.putChar(trigram._1)
+    b.putChar(trigram._2)
+    b.putChar(trigram._3)
+
+  b.putInt(nice.size)
+
+  nice.foreach: (trigram, locations) =>
+    b.putInt(trigramIds(trigram))
+    b.putInt(locations.size)
+    locations.foreach: loc =>
+      b.putInt(locationsIndex(loc))
+
+  b.array()
+end serialiseIndex
+
+def readIndex(loc: os.Path) =
+  deserialiseIndex(os.read.inputStream(loc))
+
+def deserialiseIndex(bytes: java.io.InputStream): Map[TRIGRAM, Set[Location]] =
+  val ds = DataInputStream(bytes)
+
+  val numLocationEntries = ds.readInt()
+
+  val locationIndexBuilder = Map.newBuilder[Int, Location]
+
+  for _ <- 0 until numLocationEntries do
+    val id = ds.readInt()
+    val line = ds.readInt()
+    val length = ds.readInt()
+    val strBytes = String(ds.readNBytes(length))
+
+    locationIndexBuilder += id -> Location(os.Path(strBytes), line)
+  end for
+
+  val locationIndex = locationIndexBuilder.result()
+
+  val numTrigrams = ds.readInt()
+  val trigramsBuilder = Map.newBuilder[Int, TRIGRAM]
+  for _ <- 0 until numTrigrams do
+    val id = ds.readInt()
+    val c1 = ds.readChar()
+    val c2 = ds.readChar()
+    val c3 = ds.readChar()
+    val trigram = TRIGRAM(c1, c2, c3)
+
+    trigramsBuilder += id -> trigram
+  end for
+
+  val trigrams = trigramsBuilder.result()
+
+  val indexBuilder = Map.newBuilder[TRIGRAM, Set[Location]]
+  val numOccurrences = ds.readInt()
+  for _ <- 0 until numOccurrences do
+    val id = ds.readInt()
+    val cnt = ds.readInt()
+    val locIds = Set.tabulate(cnt)(_ => ds.readInt())
+
+    indexBuilder += trigrams(id) -> locIds.map(locationIndex.apply)
+  end for
+
+  indexBuilder.result()
+end deserialiseIndex
+
+@main def index(pathInput: String, extensions: String) =
   val acc = collection.mutable.Map
     .empty[TRIGRAM, ArrayBuilder[Location]]
 
@@ -107,19 +239,32 @@ end IndexEntry
 
   val ext = extensions.split(",").toSet
 
-  os.walk.stream
-    .attrs(
-      path = os.Path(path, os.pwd),
-      skip = (p, s) => s.isFile && !ext.contains(p.ext)
-    )
-    .foreach: (path, info) =>
-      if info.isFile then
-        val file = indexer.indexFile(path)
-        file.foreach: entry =>
-          entry.tokens.foreach: tok =>
-            acc.getOrElseUpdate(tok, Array.newBuilder).addOne(entry.location)
+  def indexFile(path: os.Path, info: os.StatInfo) =
+    if info.isFile then
+      val file = indexer.indexFile(path)
+      file.foreach: entry =>
+        entry.tokens.foreach: tok =>
+          acc.getOrElseUpdate(tok, Array.newBuilder).addOne(entry.location)
+    end if
+  end indexFile
 
-  // var stop = false
+  pathInput match
+    case "-" =>
+      io.Source
+        .fromInputStream(System.in)
+        .getLines()
+        .map(os.Path(_, os.pwd))
+        .filter(p => ext.contains(p.ext))
+        .map(path => path -> os.stat(path))
+        .foreach(indexFile)
+    case _ =>
+      os.walk.stream
+        .attrs(
+          path = os.Path(pathInput, os.pwd),
+          skip = (p, s) => s.isFile && !ext.contains(p.ext)
+        )
+        .foreach(indexFile)
+  end match
 
   val fin = acc.map((tg, loc) => tg -> loc.result().toSet).toMap
 
@@ -140,6 +285,21 @@ end IndexEntry
       .sortBy(_._2 * -1)
   end search
 
-  search("TrigramIndexer").take(10).foreach(println)
+  val b = serialiseIndex(fin)
+
+  val newValue = os.pwd / "index.trig"
+  os.write.over(newValue, b)
+  println(readIndex(newValue))
+
+  // var exit = false
+
+  // while !exit do
+  //   print("> ")
+  //   val str = io.StdIn.readLine().trim()
+  //   exit = str.equalsIgnoreCase(":exit")
+
+  //   if !exit then search(str).take(10).foreach(println)
+
+  // end while
 
 end index
